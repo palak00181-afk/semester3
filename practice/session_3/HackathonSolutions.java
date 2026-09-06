b10258e9ce877f091255eeba2217fb9ab8fa7f72
palak00181-afk <palak00181@gmail.com>
2026-09-06 22:27:06 +0530

    Add Session 3 practice files

---
+++ practice/session_3/HackathonSolutions.java
@@ -0,0 +1,10 @@
+package practice.session_3;
+
+public class HackathonSolutions {
+    public static void curvescores(int[] scores,int bonus){
+        for(int i=0;i<scores.length;i++){
+            scores[i]+=bonus;
+    }
+    
+}
+}
